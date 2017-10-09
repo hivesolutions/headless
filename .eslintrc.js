c5535f0b04e9aac1b17d631bcccfccc27283b5a1
joamag <joamag@gmail.com>
2017-10-09 23:24:35 +0100

    new testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
     "extends": "standard",
+    "plugins": ["mocha"],
     "rules": {
         "indent": ["warn", 4],
         "quotes": ["error", "double"],
         "semi": ["error", "always"],
-        "space-before-function-paren": ["error", "never"]
+        "space-before-function-paren": ["error", "never"],
+        "mocha/no-exclusive-tests": "error"
     }
 };
