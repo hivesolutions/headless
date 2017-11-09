35b4ca1d37732f7e15c32b3bb3206374053c7b5e
joamag <joamag@gmail.com>
2017-11-09 17:03:11 +0000

    new simple testing

--- test/simple.js
+++ test/simple.js
@@ -3,19 +3,19 @@ var base = require("../lib/util/base");
 
 describe("Array", function() {
     describe("#indexOf()", function() {
-        it("should return -1 when the value is not present", function() {
+        it("should return -1 when the value is not present", () => {
             assert.equal(-1, [1, 2, 3].indexOf(4));
         });
     });
     describe("#length", function() {
-        it("should return proper length", function() {
+        it("should return proper length", () => {
             assert.equal(3, [1, 2, 3].length);
         });
     });
 });
 
 describe("ENGINES", function() {
-    it("should not be empty", function() {
+    it("should not be empty", () => {
         assert.equal(true, Object.keys(base.ENGINES).length > 0);
     });
 });
