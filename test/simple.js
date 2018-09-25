5fc2a9f2529c7ee9124059fef3cb45145662932d
João Magalhães <joamag@gmail.com>
2018-09-25 21:17:41 +0100

    new unit testing

--- test/simple.js
+++ test/simple.js
@@ -4,18 +4,18 @@ const lib = require("../lib");
 describe("Array", function() {
     describe("#indexOf()", function() {
         it("should return -1 when the value is not present", () => {
-            assert.equal(-1, [1, 2, 3].indexOf(4));
+            assert.strictEqual(-1, [1, 2, 3].indexOf(4));
         });
     });
     describe("#length", function() {
         it("should return proper length", () => {
-            assert.equal(3, [1, 2, 3].length);
+            assert.strictEqual(3, [1, 2, 3].length);
         });
     });
 });
 
 describe("ENGINES", function() {
     it("should not be empty", () => {
-        assert.equal(true, Object.keys(lib.ENGINES).length > 0);
+        assert.strictEqual(true, Object.keys(lib.ENGINES).length > 0);
     });
 });
