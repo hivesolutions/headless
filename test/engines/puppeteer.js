4fe3c10140a72b86e4fb5e979996f31621588aea
João Magalhães <joamag@gmail.com>
2024-02-10 00:45:11 +0000

    fix: moves the engine testing to `beforeEach`
    This way it's possible to properly skip the tests when the engine is not available.

--- test/engines/puppeteer.js
+++ test/engines/puppeteer.js
@@ -4,48 +4,53 @@ const puppeteer = require("../../lib/engines/puppeteer");
 describe("Puppeteer", function() {
     this.timeout(5000);
 
-    it("should render a PDF", async () => {
+    beforeEach(async function() {
         const engine = new puppeteer.Puppeteer();
         try {
             await engine.init();
         } catch (err) {
             this.skip();
         }
-
-        const req = {
-            query: {
-                url: "https://example.com/",
-                format: "pdf"
-            },
-            body: {}
-        };
-        const res = {
-            send: function(data) {
-                this.data = data;
-            },
-            type: function(file) {
-                this.file = file;
-            }
-        };
-        await engine.render(req, res);
-        assert.ok(res.data);
-        assert.strictEqual(res.file, "pdf");
-
         await engine.destroy();
+    });
+
+    it("should render a PDF", async () => {
+        const engine = new puppeteer.Puppeteer();
+        await engine.init();
+
+        try {
+            const req = {
+                query: {
+                    url: "https://example.com/",
+                    format: "pdf"
+                },
+                body: {}
+            };
+            const res = {
+                send: function(data) {
+                    this.data = data;
+                },
+                type: function(file) {
+                    this.file = file;
+                }
+            };
+            await engine.render(req, res);
+            assert.ok(res.data);
+            assert.strictEqual(res.file, "pdf");
+        } finally {
+            await engine.destroy();
+        }
     });
 
     it("should open new page", async () => {
         const engine = new puppeteer.Puppeteer();
+        await engine.init();
         try {
-            await engine.init();
-        } catch (err) {
-            this.skip();
+            await engine._newPage();
+            const pages = await engine.instance.pages();
+            assert.strictEqual(2, pages.length);
+        } finally {
+            await engine.destroy();
         }
-
-        await engine._newPage();
-        const pages = await engine.instance.pages();
-        assert.strictEqual(2, pages.length);
-
-        await engine.destroy();
     });
 });
