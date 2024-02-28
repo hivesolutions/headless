056515a40b391b05f9aaa576d2ceb4e633411218
João Magalhães <joamag@gmail.com>
2024-02-28 10:15:20 +0000

    chore: improved magic string validation
    This way the doc contents are verified.

--- test/engines/puppeteer.js
+++ test/engines/puppeteer.js
@@ -37,12 +37,46 @@ describe("Puppeteer", function() {
             await engine.render(req, res);
             assert.ok(res.data);
             assert.strictEqual(res.file, "pdf");
+            assert.ok(res.data.slice(0, 5).equals(Buffer.from([0x25, 0x50, 0x44, 0x46, 0x2d])));
         } finally {
             await engine.destroy();
         }
     });
 
-    it("should render a jpeg", async () => {
+    it("should render a PNG", async () => {
+        const engine = new puppeteer.Puppeteer();
+        await engine.init();
+
+        try {
+            const req = {
+                query: {
+                    url: "https://example.com/",
+                    format: "png"
+                },
+                body: {}
+            };
+            const res = {
+                send: function(data) {
+                    this.data = data;
+                },
+                type: function(file) {
+                    this.file = file;
+                }
+            };
+            await engine.render(req, res);
+            assert.ok(res.data);
+            assert.strictEqual(res.file, "png");
+            assert.ok(
+                res.data
+                    .slice(0, 8)
+                    .equals(Buffer.from([0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a]))
+            );
+        } finally {
+            await engine.destroy();
+        }
+    });
+
+    it("should render a JPEG", async () => {
         const engine = new puppeteer.Puppeteer();
         await engine.init();
 
@@ -65,6 +99,7 @@ describe("Puppeteer", function() {
             await engine.render(req, res);
             assert.ok(res.data);
             assert.strictEqual(res.file, "jpeg");
+            assert.ok(res.data.slice(0, 3).equals(Buffer.from([0xff, 0xd8, 0xff])));
         } finally {
             await engine.destroy();
         }
